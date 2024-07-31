008f1e6215dbfde2c172cad213c7438000c3748a
Mello <mello@mellos-mbp.home>
2024-07-31 16:56:39 +0200

    Rename :composeApp to :app

--- app/src/commonMain/kotlin/App.kt
+++ app/src/commonMain/kotlin/App.kt
@@ -11,8 +11,8 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import refiner.composeapp.generated.resources.Res
-import refiner.composeapp.generated.resources.compose_multiplatform
+import refiner.app.generated.resources.Res
+import refiner.app.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
