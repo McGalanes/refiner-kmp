3f88c6efd36950d3d03dc3e6a341a0a144295ce8
Mello <mello@mellos-mbp.home>
2024-08-07 13:06:04 +0200

    Support safeareas

--- app/src/commonMain/kotlin/App.kt
+++ app/src/commonMain/kotlin/App.kt
@@ -1,4 +1,7 @@
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.github.mcgalanes.refiner.core.design.theme.RefinerTheme
@@ -12,6 +15,10 @@ fun App(
     viewModel: AppViewModel = koinViewModel(),
 ) {
     RefinerTheme(darkTheme = false) {
-        CreateUserStoryScreen(Modifier.fillMaxSize())
+        CreateUserStoryScreen(
+            Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.safeDrawing)
+        )
     }
 }
